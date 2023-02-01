5c2cddcc093ab839821004c38d705756a46e9ccc
Alan Galeano <alangalea999@gmail.com>
2023-02-01 10:27:21 -0300

    UDP login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,19 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center" style="margin-top: 10%">
-        <div class="col-md-6">
+        <div class="col-md-3">
             <div class="card">
-                <div class="card-header">Sistema de Registros</div>
+                <div class="card-header text-center" style="background-color: rgb(62, 62, 62)"><span style="color: white">Sistema de Registros</span></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="row mb-3">
-                            <label for="username" class="col-md-4 col-form-label text-md-end">Nombre de Usuario</label>
-
-                            <div class="col-md-6">
-                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" autocomplete="username" autofocus>
+                        <div class="row mb-4 mt-2">
+                            <div class=" col-md-12">
+                                <input placeholder="Usuario..." id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" autocomplete="username" autofocus>
 
                                 @error('username')
                                     <span class="invalid-feedback" role="alert">
@@ -24,11 +22,9 @@
                                 @enderror
                             </div>
                         </div>
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">Email</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        <div class="row mb-4">
+                            <div class="col-md-12">
+                                <input placeholder="Email..." id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -38,11 +34,9 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">Contraseña</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                        <div class="row mb-4">
+                            <div class="col-md-12">
+                                <input placeholder="Contraseña..." id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -52,10 +46,10 @@
                             </div>
                         </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                        <div class="row mb-0" style="margin-left: 57px">
+                            <div class="col-md-4">
+                                <button type="submit" class="btn" style="background-color: rgb(62, 62, 62)">
+                                    <span style="color: white">{{ __('Login') }}</span>
                                 </button>
                             </div>
                         </div>
